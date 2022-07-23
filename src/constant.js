d64230642edac99c243e8e51fe294c32b97af91d
Vuong Tran <vuong@Vuongs-MacBook-Pro.local>
2022-07-23 17:30:14 +0700

    a

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 const PORT= process.env.PORT || 5000;
 //const Domain= "https://doannhom12.herokuapp.com"
-const Domain= "https://fashoin-sport-shop-node.herokuapp.com"
+const Domain= "https://backend-nodejs-shops.herokuapp.com"
 export const domain= `${Domain}`
